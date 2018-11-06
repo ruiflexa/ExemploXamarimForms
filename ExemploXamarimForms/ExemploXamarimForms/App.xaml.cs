37ef6ac04fa58e9d0f62e86f71563b2e2625246d
Rui Flexa <ruiflexa@yahoo.com.br>
2018-11-06 17:58:37 -0200

    Exercicios Aula 06/11

--- ExemploXamarimForms/ExemploXamarimForms/App.xaml.cs
+++ ExemploXamarimForms/ExemploXamarimForms/App.xaml.cs
@@ -11,7 +11,7 @@ namespace ExemploXamarimForms
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+		    MainPage = new MainPage();
 		}
 
 		protected override void OnStart ()
---
+++ ExemploXamarimForms/ExemploXamarimForms/Exemplo.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExemploXamarimForms
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Exemplo : ContentPage
+	{
+		public Exemplo ()
+		{
+			InitializeComponent ();
+		}
+
+	    private void BtnEnviar_OnClicked(object sender, EventArgs e)
+	    {
+	        int n = Convert.ToInt32(tbxTeste.Text);
+	        if (n % 2 == 0)
+	        {
+	           lblResult.Text += " par";
+
+	        }
+	        else
+	        {
+	           lblResult.Text += " ímpar";
+            }
+	    }
+	}
+}
